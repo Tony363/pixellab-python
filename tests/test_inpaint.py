8688c4d01a0497d7163dc4c6995c6795ba8bf65e
Tony <thefool363@gmail.com>
2025-05-18 02:01:35 -0400

    alot of tests

--- tests/test_inpaint.py
+++ tests/test_inpaint.py
@@ -5,18 +5,24 @@ from pathlib import Path
 import PIL.Image
 
 import pixellab
+from dotenv import load_dotenv
+import os
 
 
 def test_inpaint():
-    client = pixellab.Client.from_env_file(".env.development.secrets")
+    load_dotenv("../.env")
 
+    client = pixellab.Client(secret=os.getenv("PIXELLAB_API_KEY"))
+    resize_dim = (128, 128)
+    image_name = "boy.png"
+    mask_name = "mask.png"
     images_dir = Path("tests") / "images"
-    inpainting_image = PIL.Image.open(images_dir / "boy.png").resize((16, 16))
-    mask_image = PIL.Image.open(images_dir / "mask.png").resize((16, 16))
+    inpainting_image = PIL.Image.open(images_dir / image_name).resize(resize_dim)
+    mask_image = PIL.Image.open(images_dir / mask_name).resize(resize_dim)
 
     response = client.inpaint(
         description="boy with wings",
-        image_size={"width": 16, "height": 16},
+        image_size=dict(width=resize_dim[0], height=resize_dim[1]),
         no_background=True,
         inpainting_image=inpainting_image,
         mask_image=mask_image,
@@ -25,9 +31,16 @@ def test_inpaint():
 
     image = response.image.pil_image()
     assert isinstance(image, PIL.Image.Image)
-    assert image.size == (16, 16)
+    assert image.size == resize_dim
 
     results_dir = Path("tests") / "results"
     results_dir.mkdir(exist_ok=True)
 
-    image.save(results_dir / "inpainting_boy_with_wings.png")
+    image.save(
+        results_dir
+        / f"inpainting_{image_name.split('.')[0]}_with_{mask_name.split('.')[0]}.png"
+    )
+
+
+if __name__ == "__main__":
+    test_inpaint()
--- tests/test_rotate.py
+++ tests/test_rotate.py
@@ -12,16 +12,22 @@ import os
 def test_rotate():
     load_dotenv("../.env")
     client = pixellab.Client(secret=os.getenv("PIXELLAB_API_KEY"))
+    resize_dim = (128, 128)
+    image_name = "fire.jpeg"
+    from_direction = "south"
+    to_direction = "east"
+    from_view = "side"
+    to_view = "side"
     images_dir = Path("tests") / "images"
-    reference_image = PIL.Image.open(images_dir / "boy.png").resize((16, 16))
+    reference_image = PIL.Image.open(images_dir / image_name).resize(resize_dim)
     init_image = reference_image
 
     response = client.rotate(
-        from_direction="south",
-        from_view="side",
-        to_direction="north",
-        to_view="side",
-        image_size={"width": 16, "height": 16},
+        from_direction=from_direction,
+        from_view=from_view,
+        to_direction=to_direction,
+        to_view=to_view,
+        image_size={"width": resize_dim[0], "height": resize_dim[1]},
         image_guidance_scale=7.5,
         from_image=reference_image,
         init_image=init_image,
@@ -30,12 +36,15 @@ def test_rotate():
 
     image = response.image.pil_image()
     assert isinstance(image, PIL.Image.Image)
-    assert image.size == (16, 16)
+    assert image.size == resize_dim
 
     results_dir = Path("tests") / "results"
     results_dir.mkdir(exist_ok=True)
 
-    image.save(results_dir / "rotation_boy_south_to_north.png")
+    image.save(
+        results_dir
+        / f"rotation_{image_name.split('.')[0]}_{from_direction}_to_{to_direction}.png"
+    )
 
 
 if __name__ == "__main__":
